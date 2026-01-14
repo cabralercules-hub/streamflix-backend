532093ae7cddd221d7595e5659f14a28b2b22659
cabralercules-hub <cabralercules@gmail.com>
2026-01-14 20:18:28 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Servidor StreamFlix Online");
+});
+
+app.post("/criar-pagamento", async (req, res) => {
+    const { email, plano } = req.body;
+
+    const valores = {
+        mensal: 5000,
+        anual: 50000
+    };
+
+    try {
+        const resposta = await axios.post(
+            "https://api.flutterwave.com/v3/payments",
+            {
+                tx_ref: Date.now(),
+                amount: valores[plano],
+                currency: "AOA",
+                redirect_url: "https://google.com",
+                customer: { email },
+                customizations: {
+                    title: "StreamFlix",
+                    description: `Plano ${plano}`
+                }
+            },
+            {
+                headers: {
+                    Authorization: `Bearer SUA_SECRET_KEY_AQUI`
+                }
+            }
+        );
+
+        res.json(resposta.data);
+    } catch (err) {
+        res.status(500).json({ erro: "Erro ao criar pagamento" });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor ligado"));
